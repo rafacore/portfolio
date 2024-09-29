b094dbd8ea9cf7c44047c459f8484b5ad0a32b14
rafael <rafael@192-168-1-100.tpgi.com.au>
2024-09-29 22:42:33 +1000

    create others sections

---
+++ app/components/main/Experience.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <section
+    id="experience"
+    className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
+    style={{ transform: "scale(0.9)" }}
+  >
+    <div> Experience  </div>
+      </section>
+  )
+}
+
+export default Experience
