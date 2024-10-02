be4737111b3bbea8ffdeb866f2823747c47fde4b
rafael <rafael@192-168-1-100.tpgi.com.au>
2024-10-02 19:41:37 +1000

    update experience section

--- app/components/main/Experience.tsx
+++ app/components/main/Experience.tsx
@@ -11,11 +11,12 @@ const Experience = () => {
       className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20"
       style={{ transform: "scale(0.9)" }}
     >
-      <TitleSection title="Experiences" subTitle="My last jobs" />
+      <TitleSection title="Experiences" subTitle="My last jobs" featuredText="7+ years of experience"/>
       <TimeLine />
 
-      <div className="flex w-full items-center justify-center">
-        <Button label="Resume" />
+      <div className="flex w-full items-center justify-center gap-10 mt-20">
+        <Button label="Resume " />
+        <Button label="Contact Me" />
       </div>
     </section>
   );
